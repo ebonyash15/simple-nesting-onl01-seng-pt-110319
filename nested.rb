732de05e30b85919fd4ffe07c590f0fa9d0a3ccd
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-15 01:16:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,22 +1,16 @@
 
 def hopper
 	programmer_hash = 
- 		{
-        :grace_hopper => {
+ 		{   :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
+          :languages => ["COBOL", "FORTRAN"]  },
         :alan_kay => {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
+          :languages => ["Smalltalk", "LISP"]  },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-
+          :languages => ["C"]  } }
+	programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
